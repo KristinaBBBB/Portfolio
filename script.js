7a6cf89a618121afd231ec0cadc30643f6a69798
KristinaBBBB <kristina@batrak.ca>
2026-07-29 17:26:37 -0400

    Making title 1 line - not stacked

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const partialBlurb = "I build responsive and adaptable software solutions to rea
 const fullBlurb = partialBlurb + ", robotics and personal applications where I’ve focused on creating an intuitive and accessible user experience. I enjoy learning new technologies to improve the efficiency and user experience of my solutions.";
 
 const blurb = document.getElementById("blurb");
+const blurbBtn = document.getElementById("blurb-btn");
+let blurbShort = true;
+
+blurbBtn.addEventListener("click", () => {
+    if(blurbShort){
+        blurbShort = false;
+        blurbBtn.textContent = "Collapse";
+        blurb.textContent = fullBlurb;
+    }
+    else{
+        blurbShort = true;
+        blurbBtn.textContent = "Learn More";
+        blurb.textContent = partialBlurb;
+    }
+})
