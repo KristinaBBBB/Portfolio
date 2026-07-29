c090ac7fbdee4ea93b6cb9f224ceb270beb48a8e
KristinaBBBB <kristina@batrak.ca>
2026-07-29 16:25:31 -0400

    Adding javascript for blurb length adjustment

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const partialBlurb = "I build responsive and adaptable software solutions to real-world problems. My work spans client websites";
+const fullBlurb = partialBlurb + ", robotics and personal applications where I’ve focused on creating an intuitive and accessible user experience. I enjoy learning new technologies to improve the efficiency and user experience of my solutions.";
+
+const blurb = document.getElementById("blurb");
